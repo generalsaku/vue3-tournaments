8fa0c5409a3e8a669ee549f590a65afbf04c8e67
generalsaku <josefgdev@gmail.com>
2022-11-12 01:50:38 +0100

    loader for tournament

--- src/views/app/tournament/AppTournament.vue
+++ src/views/app/tournament/AppTournament.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="flex-1 overflow-auto">
+  <div class="flex-1 overflow-auto" :class="{ relative: !$store.tournament }">
     <RouterView v-if="$store.tournament"></RouterView>
+    <mdi:progress-download
+      v-else
+      class="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 text-9xl animate-pulse"
+    />
   </div>
   <AppTournamentBar></AppTournamentBar>
 </template>
